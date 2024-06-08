c1b69de107b6207bae193be1f1140fe80723dde3
zain raza <walisufi0203@gmail.com>
2024-06-08 14:07:05 +0500

    class

---
+++ list.py
@@ -0,0 +1,112 @@
+# friuts=["apple","orange","mango"]
+# sodt_drink=["pespi","7up","colanext"]
+# age_group=[20,25,100,15]
+# student_info=["danish","zain","zain",30,True,60.5,None,["Newlist","newlist1"]]
+# print("fruits",friuts)
+# print("sodt",sodt_drink)
+# print("age",age_group)
+# print("student",student_info)
+# print(student_info[5])
+# print(student_info[5])
+
+
+# print(len(student_info)-1)
+# print(student_info)
+# print(len(student_info[1]))
+
+# List_elemt_index=len(student_info)-1
+# print(student_info)
+# print(student_info[-1])
+
+# print(student_info[-3])
+# print(len(student_info-3))
+# print(student_info,len(student_info-3))
+# student_info[0]="zain"
+# print(student_info)
+# student_info[4]="ali"
+# print(student_info)
+# print(len(student_info-2="ali"))
+# del(student_info[0])
+# print(student_info)
+# last_element_index=
+# student_info.append("zain")
+# print(student_info)
+# student_info.pop()
+# print(student_info)
+# list_2=[1,2,30]
+# print()
+# list3=student_info+list_2
+# list_2.extend(student_info)
+# print(list_2)
+# list_2.reverse()
+# list_2.sort(reverse=True)
+# student_info.index("danish")
+# print(student_info.index("danish"))
+# print(student_info.remove("danish"))
+# student_info.clear()
+# student_info
+# print(student_info.count("zain"))
+# student_info.insert(2,"myzain")
+# print(student_info)
+# juices=[]
+# juices.append("apple")
+# juices.append("mango")
+# juices.append("banana")
+# juices.append("orange")
+# juices.append("cholate")
+# print(juices)
+# cars=["bmw","khyber","mehran"]
+# cars.insert(0,"cultaz")
+# cars.insert(1,"audi")
+# cars.insert(2,"ferrari")
+# print(cars)
+# list1=["bed","chair","sofa"]
+
+
+# list1.remove("chair")
+# del(list1[-2])
+
+# print(list1)
+# furniture=["bed","chair","table","sofa"]
+# furniture.remove("chair")
+# print(furniture)
+
+
+# furniture=["bed","chair","table","sofa"]
+
+# chair_index=furniture.index("chair")
+# del furniture(chair_index)
+# print(chair_index)
+furniture=["bed","chair","table","sofa"]
+# print("bed" in furniture)
+# furniture.splice
+
+
+# car_list=["coralla","mehran","cultaz","aqua","civic","witz","alto"]
+# print(car_list)
+# # print(car_list[2:5])
+# # print(car_list[3:])
+
+# x="hello world"
+# print(x[4:10])
+
+
+# stundent_tr="dainsh,30,60.5,true"
+# deail_list=stundent_tr.split(",")
+# print(deail_list)
+# print(",".join(deail_list))
+
+
+# studnets=["dainsh","imran","huzefa","abdullah","marjis"]
+# # for x in studnets:
+# #     print(x)
+
+
+# # print(list(range(10)))
+# # print(range(len(studnets)))
+# for x in range(len(studnets)):
+#     print(studnets[x])
+
+for x in furniture:
+    if "chair" in furniture:
+        print(x)
